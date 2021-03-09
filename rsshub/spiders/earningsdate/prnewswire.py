940dc1fb8241eaf16ac2e61497027780cfbffb1b
Hiller Liao <hillerliao@163.com>
2021-03-09 17:20:00 +0800

    fix prnewswire title parser

--- rsshub/spiders/earningsdate/prnewswire.py
+++ rsshub/spiders/earningsdate/prnewswire.py
@@ -4,7 +4,7 @@ domain = 'https://www.prnewswire.com'
 
 def parse(post):
     item = {}
-    item['title'] = post.css('a::text').extract_first()
+    item['title'] = post.css('h3::text').getall()[1]
     item['description'] = post.css('p::text').extract_first()
     item['link'] = f"{domain}{post.css('a::attr(href)').extract_first()}"
     item['pubDate'] = post.css('small::text').extract_first()
@@ -14,7 +14,7 @@ def ctx(category=''):
     # DEFAULT_HEADERS.update({'upgrade-insecure-requests': 1})
     url = f"{domain}/news-releases/financial-services-latest-news/earnings-list/?page=1&pagesize=100"
     tree = fetch(url, headers=DEFAULT_HEADERS)
-    posts = tree.css('.card-list-hr .col-sm-8')
+    posts = tree.css('.card-list-hr .row')
     items = list(map(parse, posts)) 
     items = filter_content(items)
     return {
