ad2f2478338b6039fd9bd748c442db35abcfac31
hillerliao <hillerliao@163.com>
2023-02-26 20:58:08 +0800

    add mp recommended tag

--- rsshub/blueprints/main.py
+++ rsshub/blueprints/main.py
@@ -204,6 +204,11 @@ def mp_tag(mp='', tag=''):
     from rsshub.spiders.mp.tag import ctx
     return render_template('main/atom.xml', **filter_content(ctx(mp,tag)))
 
+@bp.route('/mp/rtag/<string:c1>/<string:tag>')
+def mp_rtag(c1='', tag=''):
+    from rsshub.spiders.mp.rtag import ctx
+    return render_template('main/atom.xml', **filter_content(ctx(c1, tag)))
+
 @bp.route('/producthunt/search/<string:keyword>/<string:period>')
 def producthunt_search(keyword='', period=''):
     from rsshub.spiders.producthunt.search import ctx
---
+++ rsshub/spiders/mp/rtag.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import pyjsparser
+
+domain = 'https://mp.weixin.qq.com'
+
+
+def parse(post):
+    item = {}
+    item['title'] = post['properties'][4]['value']['value']  
+    item['description'] = post['properties'][5]['value']['value']      
+    item['link'] = post['properties'][6]['value']['value']      
+    item['pubDate'] = post['properties'][16]['value']['left']['value']
+    test = item['author'] = post['properties'][1]['value']['properties'][0]['value']['value']     
+    return item
+
+def ctx(c1='', tag=''):
+    url = f"{domain}/mp/recommendtag?c1={c1}&tag={tag}&msg_type=1&sn=2fGf6B-xDlazPj5_t_KgEH0Gpkw"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    scripts = soup.findAll("script")[12].text        
+    scripts = scripts.split('mp_msgs: ')[-1].split('isSubscribed')[0][:-6]
+    posts = pyjsparser.parse(scripts)['body'][0]['expression']['elements']
+    
+    return {
+        'title': f'{tag} - 微信公众号推荐话题',
+        'link': url,
+        'description': f'{tag} - 微信公众号推荐话题',
+        'author': 'hillerliao',
+        'items': list(map(parse, posts)) 
+    }
