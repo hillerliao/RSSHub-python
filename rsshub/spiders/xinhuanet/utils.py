b9b2fe90dff054c9d8c8391bd2920d1755615aa4
Zhihai Liao <hillerliao@163.com>
2023-07-01 22:23:25 +0000

    fix xpath error in  xinhuanet world

--- rsshub/spiders/xinhuanet/utils.py
+++ rsshub/spiders/xinhuanet/utils.py
@@ -1,3 +1,4 @@
+from time import sleep
 from rsshub.utils import DEFAULT_HEADERS, fetch
 
 
@@ -5,10 +6,11 @@ def parse_html(post):
     item = {}
     item['title'] = post.xpath('text()').extract_first()
     item['link'] = post.xpath('@href').extract_first()
-    print(item['link'])
+    html = fetch(item['link'], headers=DEFAULT_HEADERS)
     item['description'] = (
-        fetch(item['link'], headers=DEFAULT_HEADERS)
-        .xpath('//div[@id=\'detail\']')
+        html
+        .xpath('//div[@id="detail"]')
         .get()
     )
+    sleep(1)
     return item
