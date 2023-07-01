fec81fea0560494ece72caf1258d9db2a1c6fe6b
Hiller Liao <hillerliao@163.com>
2023-07-02 06:00:41 +0800

    remove tmp log for xinhuanet world

--- rsshub/spiders/xinhuanet/world.py
+++ rsshub/spiders/xinhuanet/world.py
@@ -7,8 +7,6 @@ domain = 'http://www.news.cn/world/index.html'
 def ctx():
     url = f'{domain}'
     tree = fetch(url, headers=DEFAULT_HEADERS)
-    with open('/home/icedragon/tmp.html', 'w') as log:
-        log.write(tree.getall()[0])
     posts = tree.xpath('//div[@id=\'recommendDepth\']//a')
     return {
         'title': '新华网 - 国际要闻',
