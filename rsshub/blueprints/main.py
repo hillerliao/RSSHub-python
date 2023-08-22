567f437cd960e8ea43be68aedba5c17a8c28221b
sillyboy110 <1521925504@qq.com>
2023-08-22 12:17:57 +0800

    Update main.py

--- rsshub/blueprints/main.py
+++ rsshub/blueprints/main.py
@@ -20,21 +20,6 @@ def feeds():
 
 
 @bp.app_template_global()
-def filter_content_bak(ctx):
-    include_title = request.args.get('include_title')
-    include_description = request.args.get('include_description')
-    exclude_title = request.args.get('exclude_title')
-    exclude_description = request.args.get('exclude_description')
-    limit = request.args.get('limit', type=int)
-    items = ctx['items'].copy()
-    items = [item for item in items if include_title in item['title']] if include_title else items
-    items = [item for item in items if include_description in item['description']] if include_description else items
-    items = [item for item in items if exclude_title not in item['title']] if exclude_title else items
-    items = [item for item in items if exclude_description not in item['description']] if exclude_description else items
-    items = items[:limit] if limit else items
-    ctx = ctx.copy()
-    ctx['items'] = items
-    return ctx
 def filter_content(ctx):
     items = ctx['items'].copy()
     if 'include_title' in request.args:
