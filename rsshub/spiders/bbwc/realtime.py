08f26c004fb155ba070083f5fc0f36b05c8d6191
HenryLoveMiller <119033012+HenryLoveMiller@users.noreply.github.com>
2024-12-22 16:18:40 +0800

    Update realtime.py

--- rsshub/spiders/bbwc/realtime.py
+++ rsshub/spiders/bbwc/realtime.py
@@ -22,7 +22,7 @@ def ctx(category=''):
     print(posts)
     posts = json.loads(posts.text)['data']['list']           
     return {
-        'title': f'{category} - 即时新闻 - 商业周刊',
+        'title': f'即时新闻 - 商业周刊',
         'link': f'{domain}/realtime/index.html',
         'description': f'抓取彭博商业周刊即时新闻栏目的快讯',
         'author': 'hillerliao',
