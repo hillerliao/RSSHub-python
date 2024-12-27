9ec16a9f8285193522a04a3a64eccfbcbcda93a7
HenryLoveMiller <119033012+HenryLoveMiller@users.noreply.github.com>
2024-12-27 11:19:00 +0800

    Update subject.py

--- rsshub/spiders/cls/subject.py
+++ rsshub/spiders/cls/subject.py
@@ -9,7 +9,7 @@ def parse(post):
     item['title'] = post['ArticleTitle']
     item['description'] = post['ArticleBrief']
     articleid = post['ArticleId']
-    item['link'] = f'https://api3.cls.cn/share/article/{articleid}?os=android&sv=734&app='
+    item['link'] = f'https://m.cls.cn/detail/{articleid}'
     item['author'] = post['ArticleAuthor']
     item['pubDate'] = arrow.get(int(post['ArticleTime'])).isoformat()
     return item
@@ -23,7 +23,7 @@ def ctx(category=''):
     items = list(map(parse, posts))
     return {
         'title': f'{category} - 主题 - 财联社',
-        'link': f'https://api3.cls.cn/share/subject/{category}?os=android&sv=734',
+        'link': f'https://www.cls.cn/subject/{category}',
         'description': f'{category} - 主题 - 财联社',
         'author': 'hillerliao',
         'items': items
