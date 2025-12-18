a829557af864fd4a2f69d0a7e2f3cb3ee9e77fff
RSSHub-Py <rsshub-py@example.com>
2025-12-19 01:22:35 +0800

    fix: Improve link handling in parse_html function to support xinhua's relative URLs

--- rsshub/spiders/xinhuanet/utils.py
+++ rsshub/spiders/xinhuanet/utils.py
@@ -4,7 +4,13 @@ from rsshub.utils import DEFAULT_HEADERS, fetch
 def parse_html(post):
     item = {}
     item['title'] = post.get_text()
-    item['link'] = post['href']
+    href = post['href']
+    # Handle relative URLs
+    if href.startswith('http'):
+        item['link'] = href
+    else:
+        # Convert relative URL to absolute URL
+        item['link'] = f'http://www.news.cn/{href}'
     print(item['link'])
     detail_tree = fetch(item['link'], headers=DEFAULT_HEADERS)
     detail_elem = detail_tree.select('#detail') if detail_tree else []
